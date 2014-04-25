8eff48bc531a3516e7e6d98c7e62f7495ad73dec
Michiel Lowijs <mlowijs@gmail.com>
2014-04-25 13:30:43 +0200

    Refactored State enum to 0xnn syntax.

--- Nrf8001Lib/Nrf8001State.cs
+++ Nrf8001Lib/Nrf8001State.cs
@@ -3,12 +3,12 @@ namespace Nrf8001Lib
 {
     public enum Nrf8001State : byte
     {
-        Test = 1,
-        Setup = 2,
-        Standby = 3,
+        Test = 0x01,
+        Setup = 0x02,
+        Standby = 0x03,
 
         // Custom
-        Unknown = 0,
-        Resetting = 255,
+        Unknown = 0x00,
+        Resetting = 0xFF,
     }
 }
