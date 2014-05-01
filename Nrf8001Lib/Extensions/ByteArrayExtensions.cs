2746d0f4242b3e4dbe73a65cd669ac14752b5065
Michiel Lowijs <mlowijs@gmail.com>
2014-05-01 22:14:55 +0200

    Added some docs for ByteArrayExtensions

--- Nrf8001Lib/Extensions/ByteArrayExtensions.cs
+++ Nrf8001Lib/Extensions/ByteArrayExtensions.cs
@@ -5,12 +5,22 @@ namespace Nrf8001Lib.Extensions
 {
     public static class ByteArrayExtensions
     {
-        public static ulong ToUnsignedLong(this byte[] bytes, int start, int length = 8)
+        /// <summary>
+        /// Converts an array of bytes into an unsigned long, least significant byte first.
+        /// </summary>
+        /// <param name="data">The data to create an unsigned long from.</param>
+        /// <param name="start">The location in the data to start converting from.</param>
+        /// <param name="length">The amount of bytes to convert.</param>
+        /// <returns></returns>
+        public static ulong ToUnsignedLong(this byte[] data, int start = 0, int length = 8)
         {
+            if (length < 1 || length > 8)
+                throw new ArgumentOutOfRangeException("length", "Length must be between 0 and 9.");
+
             ulong result = 0;
 
             for (int i = 0; i < length; i++)
-                result |= (ulong)((ulong)bytes[start + i] << i * 8);
+                result |= (ulong)((ulong)data[start + i] << i * 8);
 
             return result;
         }
